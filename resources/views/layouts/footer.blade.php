6bac416c163b9b39a3cbf9dcf08e0df8a706ad3d
striderxfossility <y%^w7W%8Hc%t>
2022-10-10 17:11:50 +0200

    added analytics

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -89,3 +89,13 @@ function news()
     });
 }
 </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149641362-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-149641362-1');
+</script>
