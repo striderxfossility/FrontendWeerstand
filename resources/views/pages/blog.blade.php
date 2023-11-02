785373578ece7b57fcff33940c00c383ada0f64f
jellebakker15@live.nl <jellebakker15@live.nl>
2023-11-02 19:57:43 +0100

    remove blok

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -2,15 +2,6 @@
     <div>
         <div id="top">&nbsp;</div>
         <div class="lg:w-2/3 m-auto">
-            <div class="hidden lg:block">
-                <x-moduleTitle
-                    name="Collectie" 
-                    textA="De natuur heeft een enorme diversiteit aan natuursteen voor ons. Er is zoveel moois op deze aarde te vinden. Elk materiaal heeft bijzondere eigenschappen; keihard, tijdloos of een duizelingwekkend patroon? De aarde ontvouwt haar schatten in onze collectie van marmer, kwartsiet en graniet.
-                            Wij zijn naast gepassioneerde natuursteenbewerkers ook trotse dealer van o.a. Dekton, Neolith en Silestone. "
-                    textB="Esthetisch zijn deze materialen prachtig en het onderhoud is eenvoudiger dan natuursteen. Het materiaal stelt u in staat uw design dromen waar te maken! Het is niet voor niets dat architecten en interieurstylisten graag met deze materialen werken."
-                />
-            </div>
-
             <div class="lg:grid 3xl:grid-cols-4 xl:grid-cols-3 grid-cols-2 gap-20">
                 @foreach ($blogs as $blog)
 
@@ -28,14 +19,6 @@
                         linkTxt="{{ $blog->link_text }}"
                     />
                 @endforeach
-            </div>
-
-            <div class="block lg:hidden">
-                <ModuleTitle
-                    name="Collectie" 
-                    textA="De natuur heeft een enorme diversiteit aan natuursteen voor ons. Er is zoveel moois op deze aarde te vinden. Elk materiaal heeft bijzondere eigenschappen; keihard, tijdloos of een duizelingwekkend patroon? De aarde ontvouwt haar schatten in onze collectie van marmer, kwartsiet en graniet. "
-                    textB="Wij zijn naast gepassioneerde natuursteenbewerkers ook trotse dealer van o.a. Dekton, Neolith en Silestone. Op de vlakken van esthetiek en onderhoudsgemak zijn deze merken steengoed. Het materiaal stelt u in staat u design dromen waar te maken! Het is niet voor niets dat architecten en interieurstylisten graag met deze materialen werken."
-                />
             </div>
         </div>
     </div>
